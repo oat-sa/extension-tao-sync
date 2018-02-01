9b6479229109ef902a18e4f4efd7bd8ee4bebc41
Dieter Raber <dieter@taotesting.com>
2018-02-01 16:08:56 +0100

    bump version

--- manifest.php
+++ manifest.php
@@ -23,7 +23,7 @@ return array(
     'label' => 'Tao Sync',
     'description' => 'TAO synchronisation for offline client data.',
     'license' => 'GPL-2.0',
-    'version' => '0.0.1',
+    'version' => '0.0.2',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao' => '>=14.16.0'
