6c098ae89bfd6b17de4b8dfb6b2d7e3e5e1771b0
siwane <camille@taotesting.com>
2018-03-08 10:30:05 +0100

    Dix tao sync user service

--- model/oauth/OauthUserService.php
+++ model/oauth/OauthUserService.php
@@ -35,7 +35,7 @@ class OauthUserService extends UserService
      */
     public function createConsumerUser(\core_kernel_classes_Resource $consumer)
     {
-        $consumer = parent::createOauthUser($consumer);
+        $consumer = parent::createConsumerUser($consumer);
         $consumer->setPropertyValue($this->getProperty(GenerisRdf::PROPERTY_USER_ROLES), self::TAO_SYNC_ROLE);
         return $consumer;
     }
