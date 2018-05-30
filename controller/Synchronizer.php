73e32d824a63e23f7078dc49233d986972b6fe2b
Ionut Padurariu <ionut@taotesting.com>
2018-05-30 09:17:03 +0200

    add ability to run custom tasks

--- controller/Synchronizer.php
+++ controller/Synchronizer.php
@@ -21,9 +21,8 @@ namespace oat\taoSync\controller;
 
 use oat\generis\model\OntologyAwareTrait;
 use oat\taoSync\model\history\DataSyncHistoryService;
+use oat\taoSync\model\SynchronizeAllTaskBuilderService;
 use oat\taoSync\model\ui\FormFieldsService;
-use oat\taoSync\scripts\tool\synchronisation\SynchronizeAll;
-use oat\taoTaskQueue\model\QueueDispatcherInterface;
 use oat\taoTaskQueue\model\Task\TaskInterface;
 use oat\taoTaskQueue\model\TaskLogActionTrait;
 
@@ -63,10 +62,11 @@ class Synchronizer extends \tao_actions_CommonModule
             $label = $data['label'];
             unset($data['label']);
 
-            $callable = $this->propagate(new SynchronizeAll());
-            $queueService = $this->getServiceLocator()->get(QueueDispatcherInterface::SERVICE_ID);
-            $task = $queueService->createTask($callable, $data, $label);
+            /** @var SynchronizeAllTaskBuilderService $syncAllRunnerService */
+            $syncAllRunnerService = $this->getServiceLocator()->get(SynchronizeAllTaskBuilderService::SERVICE_ID);
+            $task = $syncAllRunnerService->run($data, $label);
             $this->setLastSyncTask($task);
+
             return $this->returnTaskJson($task);
         } catch (\Exception $e) {
             return $this->returnJson([
