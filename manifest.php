b6775bc0e3628f79e5658a084123b0cdf38f3d16
Aleh Hutnikau <hutnikau@1pt.com>
2018-09-06 13:09:42 +0300

    udpate version

--- manifest.php
+++ manifest.php
@@ -27,7 +27,7 @@ return array(
     'label' => 'Tao Sync',
     'description' => 'TAO synchronisation for offline client data.',
     'license' => 'GPL-2.0',
-    'version' => '1.6.1',
+    'version' => '1.6.2',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'generis' => '>=7.9.5',
