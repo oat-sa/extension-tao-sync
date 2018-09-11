ded3edae09668fdad5d2b478d52336cb42079095
Mikhail Kamarouski <kamarouski@1pt.com>
2018-09-11 13:12:16 +0300

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -386,7 +386,7 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('1.4.0');
         }
 
-        $this->skip('1.4.0', '1.6.2');
+        $this->skip('1.4.0', '1.6.3');
     }
 
     /**
