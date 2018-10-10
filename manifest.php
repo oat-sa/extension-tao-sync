8c3714e6500b9c45386de93ac00b2528e2dbf890
uncleempty <ilya.yarkavets@1pt.com>
2018-10-10 12:38:38 +0300

    Correct version bump, after updater fixes

--- manifest.php
+++ manifest.php
@@ -27,7 +27,7 @@ return array(
     'label' => 'Tao Sync',
     'description' => 'TAO synchronisation for offline client data.',
     'license' => 'GPL-2.0',
-    'version' => '1.6.2',
+    'version' => '1.6.2.1',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'generis' => '>=7.9.5',
