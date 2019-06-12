ea09120913c261141a04e04b098706300218e786
yuryfilipovich <yury.filipovich@1pt.com>
2019-06-12 09:43:13 +0300

    TAO-8467 - catch check service report Exception to just hide report block

--- model/OfflineMachineChecksService.php
+++ model/OfflineMachineChecksService.php
@@ -77,7 +77,11 @@ class OfflineMachineChecksService extends ConfigurableService
         $dataUsageReport->setMessage(self::REPORT_USAGE_TITLE);
 
         foreach ($this->getCheckServices() as $check) {
-            $dataUsageReport->add($check->getReport());
+            try {
+                $dataUsageReport->add($check->getReport());
+            } catch (Exception $e) {
+                $this->logError($e->getMessage());
+            }
         }
 
         return $dataUsageReport;
