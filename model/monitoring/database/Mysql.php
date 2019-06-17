8a81b8d48006922f53311016a5a78874dc43337e
yuryfilipovich <yury.filipovich@1pt.com>
2019-06-17 15:41:26 +0300

    TAO-8467 - mysql request fix

--- model/monitoring/database/Mysql.php
+++ model/monitoring/database/Mysql.php
@@ -30,7 +30,10 @@ class Mysql extends DatabaseSpaceUsageService
      */
     protected function getTargetVolume()
     {
-        $data = $this->getPersistence()->query('SHOW VARIABLES WHERE Variable_Name = \'datadir\';')->fetch();
+        $data = $this->getPersistence()->query(
+            'SHOW VARIABLES WHERE Variable_Name = :Variable_Name',
+            ['Variable_Name' => 'datadir']
+        )->fetch();
         return $data['Value'];
     }
 
