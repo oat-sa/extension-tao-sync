ea7633a7f45fe04364ab2f4ba2486da00a8f939b
Oleksandr Khitko <oleksandr@taotesting.com>
2019-09-12 15:34:38 +0200

    Version bump

--- manifest.php
+++ manifest.php
@@ -32,7 +32,7 @@ return array(
     'label' => 'Tao Sync',
     'description' => 'TAO synchronisation for offline client data.',
     'license' => 'GPL-2.0',
-    'version' => '6.13.0',
+    'version' => '6.13.1',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'generis'         => '>=7.9.5',
