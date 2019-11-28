d89d3cdc81cb700c65fb3c03ee33004b10571dba
yuryfilipovich <yury.filipovich@1pt.com>
2019-11-28 14:59:18 +0300

    bump version

---
+++ config/default/PackageService.conf.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default config header created during install
+ */
+
+return new oat\taoSync\package\PackageService(array(
+    'storage' => new oat\taoSync\package\storage\FileSystemStorageService()
+));
