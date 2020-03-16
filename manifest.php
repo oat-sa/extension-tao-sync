315c94a9d9f4cc2f19d08cb3f1806a987fb104e8
Razhkou <yuryr@taotesting.com>
2020-03-16 15:26:40 +0300

    bump. upgrade to phpunit8

--- manifest.php
+++ manifest.php
@@ -33,7 +33,7 @@ return [
     'label' => 'Tao Sync',
     'description' => 'TAO synchronisation for offline client data.',
     'license' => 'GPL-2.0',
-    'version' => '7.2.1',
+    'version' => '7.3.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => [
         'generis'         => '>=12.5.0',
