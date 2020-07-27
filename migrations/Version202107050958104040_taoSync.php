37be8b6bd27536222fdcfde9aed3847d592bd1e5
Augustas Nedzinskas <augustas@taotesting.com>
2020-07-27 10:10:13 +0200

    feat: do not recalculate checksum when storing synced entities #190
    (cherry picked from commit 8a206a62a2fb194e5abf697da615491b12d69981)

--- migrations/Version202107050958104040_taoSync.php
+++ migrations/Version202107050958104040_taoSync.php
@@ -6,6 +6,8 @@ namespace oat\taoSync\migrations;
 
 use common_report_Report as Report;
 use Doctrine\DBAL\Schema\Schema;
+use oat\generis\model\data\event\ResourceDeleted;
+use oat\oatbox\event\EventManager;
 use oat\tao\scripts\tools\migrations\AbstractMigration;
 use oat\taoSync\model\EntityChecksumCacheService;
 
@@ -14,31 +16,53 @@ final class Version202107050958104040_taoSync extends AbstractMigration
 
     public function getDescription(): string
     {
-        return 'Register ' . EntityChecksumCacheService::class;
+        return sprintf(
+            'Subscribe %s::%s() to %s event',
+            EntityChecksumCacheService::class,
+            'entityDeleted',
+            ResourceDeleted::class
+        );
     }
 
     public function up(Schema $schema): void
     {
-        $this->getServiceManager()->register(
-            EntityChecksumCacheService::SERVICE_ID,
-            new EntityChecksumCacheService(
-                [
-                    EntityChecksumCacheService::OPTION_PERSISTENCE => 'default_kv',
-                ]
-            )
-        );
+        $eventManager = $this->getEventManager();
+        $eventManager->attach(ResourceDeleted::class, [EntityChecksumCacheService::class, 'entityDeleted']);
+        $this->getServiceManager()->register(EventManager::SERVICE_ID, $eventManager);
 
         $this->addReport(
-            Report::createSuccess('Registered ' . EntityChecksumCacheService::class)
+            Report::createSuccess(
+                sprintf(
+                    'Subscribed %s::%s() to %s event',
+                    EntityChecksumCacheService::class,
+                    'entityDeleted',
+                    ResourceDeleted::class
+                )
+            )
         );
     }
 
     public function down(Schema $schema): void
     {
+        $eventManager = $this->getEventManager();
+        $eventManager->detach(ResourceDeleted::class, [EntityChecksumCacheService::class, 'entityDeleted']);
+        $this->getServiceManager()->register(EventManager::SERVICE_ID, $eventManager);
+
         $this->addReport(
-            $this->getServiceManager()->unregister(EntityChecksumCacheService::SERVICE_ID)
-                ? Report::createSuccess('Unregistered ' . EntityChecksumCacheService::class)
-                : Report::createFailure('Failed to unregister ' . EntityChecksumCacheService::class)
+            Report::createSuccess(
+                sprintf(
+                    'Unsubscribed %s::%s() from %s event',
+                    EntityChecksumCacheService::class,
+                    'entityDeleted',
+                    ResourceDeleted::class
+                )
+            )
         );
+    }
+
+    private function getEventManager(): EventManager
+    {
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+        return $this->getServiceLocator()->get(EventManager::class);
     }
 }
---
+++ scripts/install/RegisterEntityChecksumCache.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA ;
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoSync\scripts\install;
+use common_Exception;
+use common_report_Report;
+use oat\generis\model\data\event\ResourceDeleted;
+use oat\generis\model\data\event\ResourceUpdated;
+use oat\oatbox\extension\InstallAction;
+use oat\taoSync\model\EntityChecksumCacheService;
+
+/**
+ * sudo -u www-data php index.php 'oat\taoSync\scripts\install\RegisterEntityChecksumCache'
+ */
+class RegisterEntityChecksumCache extends InstallAction
+{
+    /**
+     * @param $params
+     * @return common_report_Report
+     * @throws common_Exception
+     */
+    public function __invoke($params)
+    {
+        $service = new EntityChecksumCacheService([
+            EntityChecksumCacheService::OPTION_PERSISTENCE => 'default_kv',
+        ]);
+
+        $this->registerService(EntityChecksumCacheService::SERVICE_ID, $service);
+
+        $this->registerEvent(ResourceDeleted::class, [EntityChecksumCacheService::SERVICE_ID, 'entityDeleted']);
+
+        return new common_report_Report(common_report_Report::TYPE_SUCCESS, 'EntityChecksumCacheService was registered.');
+    }
+}
